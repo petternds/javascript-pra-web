f0b856e18d002af36f7596fa5a1035cc6c8a4a09
Peter <petternds@gmail.com>
2024-10-07 06:59:54 -0300

    lista de comprados com o check

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
 
 let contador = 0;
 
@@ -29,11 +30,16 @@ function adicionarItem(evento) {
     checkboxLabel.addEventListener("click", function (evento) {
         const checkboxInput = evento.currentTarget.querySelector(".input-checkbox");
         const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo")
 
         if (checkboxInput.checked) {
             checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
         } else {
             checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.append(itemDaLista);
         }
     });
 
@@ -47,6 +53,7 @@ function adicionarItem(evento) {
     containerNomeDoItem.appendChild(containerCheckbox);
 
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem);
 
